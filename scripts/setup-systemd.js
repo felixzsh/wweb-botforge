2196de1215f7cc0eff7ee82ce589df26f868e4e8
felixzsh <felix.sanchez.dev@gmail.com>
2025-10-17 19:07:19 -0500

    rename folder from templates to systemd

--- scripts/setup-systemd.js
+++ scripts/setup-systemd.js
@@ -66,7 +66,7 @@ function setupSystemd() {
       console.log(`✅ Created systemd user directory: ${systemdUserDir}`);
     }
 
-    const templatePath = path.join(__dirname, '..', 'templates', 'wweb-botforge.service.template');
+    const templatePath = path.join(__dirname, '..', 'systemd', 'wweb-botforge.service.template');
     let serviceContent = fs.readFileSync(templatePath, 'utf8');
 
     serviceContent = serviceContent
